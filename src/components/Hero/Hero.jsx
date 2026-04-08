f246f374b7e595da04c72b60bda8ee26baadf7ec
Nallie Than <zhengyi.than@gmail.com>
2026-04-08 21:16:36 +0800

    feat: hero - add hero css styling

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,21 +1,21 @@
 import React from 'react'
+import './Hero.css'
 
 const Hero = () => {
   return (
-    <div className='relative bg-[#fff4e9] pt-36 pb-24 px-[10%] lg:px-[20%] mx-auto flex flex-col gap-14 md:gap-24'>
-      <img src="src/assets/hero-bg-1.png" alt="hero-bg" className='absolute w-[33%] -top-18 left-4 lg:w-[20%] lg:-top-18 lg:left-[20%]'/>
-      <img src="src/assets/hero-bg-2.png" alt="hero-bg" className='w-[15%] absolute bottom-0 right-[20%]'/>
+    <div className='hero relative bg-[#fff4e9] pt-36 pb-24 px-[10%] lg:px-[15%] mx-auto flex flex-col gap-14 md:gap-24'>
+      <img src="src/assets/hero-bg-1.png" alt="hero-bg" className='hero-top-image'/>
+      <img src="src/assets/hero-bg-2.png" alt="hero-bg" className='hero-bottom-image'/>
       
-
       <div className='mx-auto text-center z-10'>
-          <h1 className='text-[40px] font-semibold md:text-[66px] lg:text-[88px] text-[#5a6a5d] leading-none'>R+ Counselling Space</h1>
-          <img src="src/assets/hero-bg-3.png" alt="hero-bg" className='hidden lg:w-[50%] mx-auto'/>
+          <h1 className='text-[40px] md:text-[66px] lg:text-[80px] xl:text-[88px] mb-3 md:mb-0 font-semibold  text-[#5a6a5d] leading-none'>R+ Counselling Space</h1>
+          <img src="src/assets/hero-bg-3.png" alt="hero-bg" className='hidden md:block md:w-[50%] mx-auto'/>
           <p className='text-lg md:text-2xl text-[#55675b] leading-normal'>Professional Counselling for Adolescents & Adults</p>
       </div> 
 
       <div className='flex flex-col justify-between items-center gap-12 lg:gap-6 z-10'>
         <p className='text-lg md:text-[22px] lg:text-[26px] text-[#55675b] flex flex-col md:flex-row items-center md:gap-2'>
-          <span><i className="fa-solid fa-laptop mr-2"></i>Online Sessions Available </span>
+          <span><i className="fa-solid fa-laptop mr-2"></i>Online Sessions (Available Now) </span>
           <span className='hidden md:block'>|</span>
           <span><i className="fa-solid fa-location-dot mr-2"></i> Seremban (Coming Soon)</span>
         </p>
