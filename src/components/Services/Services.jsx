1730d87023990b326e9cf88e8c1e131cc97dc546
Nallie Than <zhengyi.than@gmail.com>
2026-04-09 22:12:27 +0800

    feat: service - background image

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -8,14 +8,18 @@ const Services = () => {
     },
     {
       title: "Adolescent Counselling (Teens)",
-      desc: ["Support for adolescents experiencing emotional difficulties, school-related stress, peer relationships, and identity development."]
+      desc: ["Support for adolescents experiencing emotional difficulties, school-related stress, peer relationships, family communication, and identity development."]
     },
     {
       title: "Parent Consultation",
       desc: ["Support for parents seeking to better understand their child or teenager’s emotional needs, behaviour, and communication patterns."]
     },
     {
-      title: "Workshop & Talks",
+      title: "Career Counselling",
+      desc: ["Support for individuals exploring career direction, study pathways, decision-making, and transitions in education or work."]
+    },
+    {
+      title: "Workshop & Talks (Upon Request)",
       desc: ["Psychoeducation and wellbeing-focused sessions for schools, organisations, or groups.","Topics are tailored based on needs."]
     }
   ];
@@ -29,33 +33,37 @@ const Services = () => {
   ]
 
   return (
-    <div className='about bg-[#fff4e9] p-24 px-[10%] lg:px-[15%] flex flex-col gap-10'>
+    <div className='about relative bg-[#fff4e9] p-24 px-[10%] lg:px-[15%] flex flex-col gap-14'>
+        <img className='absolute -top-36 -left-20 w-112.5' src="src/assets/service.png" alt="service" />
         <h2 className='relative text-[60px] text-[#fff4e9] mx-auto w-fit px-12 py-4 bg-[url("src/assets/services.png")] bg-no-repeat bg-center bg-contain'>
             Services
         </h2>
 
         <p className='text-[#55675b] text-[21px] px-[10%]'>Counselling services are available for adolescents and adults, focusing on emotional wellbeing, personal challenges, and life transitions.</p>
     
-        <div className='text-[#55675b] flex flex-wrap gap-6 justify-center '>
-          {serviceData.map((service, index) => (
-            <div key={index} className='service-container shadow-lg'>
-              <ul className="p-0 m-0">
-                <li className='service-list'>{service.title}</li>
-              </ul>
-              {service.desc.map((paragraph, pIndex) => (
-                <p key={pIndex} className='text-[21px]'>
-                  {paragraph}
-                </p>
-              ))}
-            </div>
+       <div className='flex flex-wrap gap-10 justify-evenly items-stretch'>
+        {serviceData.map((service, index) => (
+          <div 
+            key={index} 
+            className='bg-white p-8 rounded-2xl shadow-xl flex flex-col gap-4 min-w-full max-w-[50%] md:w-[30%] lg:w-[30%]'
+          >
+            <ul className="p-0 m-0">
+              <li className='service-list font-bold text-[#55675b] text-2xl'>{service.title}</li>
+            </ul>
+            {service.desc.map((paragraph, pIndex) => (
+              <p key={pIndex} className='text-[19px] text-[#55675b] leading-relaxed'>
+                {paragraph}
+              </p>
+            ))}
+          </div>
           ))}
         </div>
 
-        <div className='service-container shadow-lg w-fit mx-auto'>
+        <div className='bg-white p-8 rounded-2xl shadow-xl w-fit mx-auto flex flex-col gap-6'>
           <p className='text-3xl font-bold text-[#55675b] text-center'>Areas of Support</p>
-          <ul className='text-[21px] text-[#55675b]'>
+          <ul className='text-[21px] text-[#55675b] flex flex-col gap-4'>
             {areaOfSupport.map((support, index) => (
-              <li key={index} className='support-list mb-2'>{support}</li>
+              <li key={index} className='support-list'>{support}</li>
             ))}
           </ul>
         </div>
