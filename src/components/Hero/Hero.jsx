c9e7414a66753fa9509cc28c79d9839eb0305ece
Nallie Than <zhengyi.than@gmail.com>
2026-04-12 16:23:57 +0800

    feat: install framer motion

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,4 +1,5 @@
 import './Hero.css';
+import { motion } from "framer-motion";
 import hero1 from '../../assets/hero-bg-1.png';
 import hero2 from '../../assets/hero-bg-2.png';
 import hero3 from '../../assets/hero-bg-3.png';
@@ -7,11 +8,26 @@ import hero3 from '../../assets/hero-bg-3.png';
 const Hero = () => {
   return (
     <div id="hero" className='hero relative bg-[#fff4e9] pt-36 pb-24 px-[5%] sm:px-[10%] lg:px-[20%] mx-auto flex flex-col gap-14 md:gap-24'>
-      <img src={hero1} alt="hero-bg" className='hero-top-image'/>
+      <motion.img
+        src={hero1}
+        alt="hero-bg"
+        className="hero-top-image"
+        initial={{ rotate: -10 }}
+        animate={{ rotate: 10 }}
+        style={{ originY: 0 }}
+        transition={{ duration: 0.8, ease: "easeOut" }}
+      />
       <img src={hero2} alt="hero-bg" className='hero-bottom-image'/>
       
       <div className='mx-auto text-center z-10'>
-          <h1 className='text-4xl sm:text-[40px] md:text-[66px] lg:text-[80px] xl:text-[88px] mb-3 md:mb-0 font-semibold  text-[#5a6a5d] leading-none'>R+ Counselling Space</h1>
+          <motion.h1 
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.8, ease: "easeOut" }} 
+            className='text-4xl sm:text-[40px] md:text-[66px] lg:text-[80px] xl:text-[88px] mb-3 md:mb-0 font-semibold  text-[#5a6a5d] leading-none'>
+              R+ Counselling Space
+          </motion.h1>
+          
           <img src={hero3} alt="hero-bg" className='hidden md:block md:w-[50%] mx-auto'/>
           <p className='max-[375px]:px-10 text-lg md:text-2xl text-[#55675b] leading-normal'>Professional Counselling for Adolescents & Adults</p>
       </div> 
