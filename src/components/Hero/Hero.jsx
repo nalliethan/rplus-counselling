01d9b3e03d41794a05055c2138d4c3cf305c2421
Nallie Than <zhengyi.than@gmail.com>
2026-04-12 14:22:09 +0800

    feat: update styling

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,18 +6,18 @@ import hero3 from '../../assets/hero-bg-3.png';
 
 const Hero = () => {
   return (
-    <div id="hero" className='hero relative bg-[#fff4e9] pt-36 pb-24 px-[10%] lg:px-[20%] mx-auto flex flex-col gap-14 md:gap-24'>
+    <div id="hero" className='hero relative bg-[#fff4e9] pt-36 pb-24 px-[5%] sm:px-[10%] lg:px-[20%] mx-auto flex flex-col gap-14 md:gap-24'>
       <img src={hero1} alt="hero-bg" className='hero-top-image'/>
       <img src={hero2} alt="hero-bg" className='hero-bottom-image'/>
       
       <div className='mx-auto text-center z-10'>
-          <h1 className='text-[40px] md:text-[66px] lg:text-[80px] xl:text-[88px] mb-3 md:mb-0 font-semibold  text-[#5a6a5d] leading-none'>R+ Counselling Space</h1>
+          <h1 className='text-4xl sm:text-[40px] md:text-[66px] lg:text-[80px] xl:text-[88px] mb-3 md:mb-0 font-semibold  text-[#5a6a5d] leading-none'>R+ Counselling Space</h1>
           <img src={hero3} alt="hero-bg" className='hidden md:block md:w-[50%] mx-auto'/>
-          <p className='text-lg md:text-2xl text-[#55675b] leading-normal'>Professional Counselling for Adolescents & Adults</p>
+          <p className='max-[375px]:px-10 text-lg md:text-2xl text-[#55675b] leading-normal'>Professional Counselling for Adolescents & Adults</p>
       </div> 
 
       <div className='flex flex-col justify-between items-center gap-12 lg:gap-6 z-10'>
-        <p className='text-[17px] md:text-[22px] lg:text-[26px] text-[#55675b] flex flex-col md:flex-row items-center md:gap-2'>
+        <p className='max-[320px]:text-lg text-xl md:text-[22px] lg:text-[26px] text-[#55675b] flex flex-col md:flex-row items-center md:gap-2'>
           <span><i className="fa-solid fa-laptop mr-2"></i>Online Sessions (Available Now) </span>
           <span className='hidden md:block'>|</span>
           <span><i className="fa-solid fa-location-dot mr-2"></i> Seremban (Coming Soon)</span>
--- src/components/Session/Session.jsx
+++ src/components/Session/Session.jsx
@@ -2,12 +2,12 @@ import session from '../../assets/session.png'
 
 const Session = () => {
   return (
-    <div id="session" className='min-h-[80vh] relative bg-linear-to-r from-[#ffeddb] to-[#ffffff] text-[#55675b] text-lg sm:text-2xl flex items-center justify-center gap-16 md:gap-20 lg:gap-28 flex-col md:flex-row py-28 md:py-20 lg:py-38 px-[10%]'>
+    <div id="session" className='min-h-[80vh] relative bg-linear-to-r from-[#ffeddb] to-[#ffffff] text-[#55675b] text-lg sm:text-2xl flex items-center justify-center gap-16 md:gap-20 lg:gap-28 flex-col md:flex-row pt-20 pb-28 md:py-20 lg:py-38 px-[10%]'>
         <img className='absolute w-[70%] opacity-50 sm:opacity-100 sm:w-[40%] lg:w-[22%] xl:w-[25%] 2xl:w-[18%] bottom-0 md:left-36' src={session} alt="session" />
         
         <h1 className='text-[40px] sm:text-6xl font-bold z-10'>Session Mode</h1>
 
-        <div className='flex flex-col gap-14 lg:max-w-[45%] z-10 font-bold sm:font-normal'>
+        <div className='flex flex-col gap-10 sm:gap-14 lg:max-w-[45%] z-10 font-medium sm:font-normal'>
             <div className='flex items-center gap-3 sm:gap-6'> 
                 <div className='min-w-10 flex justify-center'> 
                     <i className="fa-solid fa-laptop"></i>
